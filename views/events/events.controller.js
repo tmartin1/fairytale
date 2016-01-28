f59a7f7985e21a579ad8e087b4f8fb921f5db0ed
Tim Martin <timothy.m.martin@gmail.com>
2016-01-27 20:51:35 -0500

    Add event info.

--- views/events/events.controller.js
+++ views/events/events.controller.js
@@ -7,7 +7,27 @@
     function eventsController () {
         var vm = this;
 
-        vm.title =  'events';
+        vm.events = [{
+            title: 'Wedding Ceremony',
+            time: '7:00 PM',
+            date: moment('20160625').format('MMMM Do YYYY'),
+            address: {
+                street: '4401 Floridian Way',
+                city: 'Lake Buena Vista, Orlando',
+                state: 'FL',
+                zip: '32830'
+            }
+        }, {
+            title: 'Wedding Reception',
+            time: '8:30 PM',
+            date: moment('20160625').format('MMMM Do YYYY'),
+            address: {
+                street: '2101 Boardwalk Resort Ballroom',
+                city: 'Orlando',
+                state: 'FL',
+                zip: '32830'
+            }
+        }]
     }
 
 })();
