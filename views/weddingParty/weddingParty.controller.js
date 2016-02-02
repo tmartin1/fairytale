50e216ddc91c20f49ba8ec32fe8e1be718e6272c
Tim Martin <timothy.m.martin@gmail.com>
2016-02-02 00:36:59 -0500

    Add background, update style.

--- views/weddingParty/weddingParty.controller.js
+++ views/weddingParty/weddingParty.controller.js
@@ -39,41 +39,33 @@
 
         vm.groomsmen = [{
             name: 'Alexander Martin',
-            title: 'Best Man, Older Brother, Sage, Guardian',
+            title: 'Best Man • Older Brother • Sage • Guardian',
             asset: 'alex'
         }, {
             name: 'Nicholas Martin',
-            title: 'Best Man, Younger Brother, Inspiration, Unparallelled Taboo Partner, ADC',
+            title: 'Best Man • Younger Brother • Inspiration • Telepathic Taboo Partner • ADC',
             asset: 'nick'
         }, {
             name: 'Christophe Van Elewyck',
-            title: 'Groomsman, Muse, Spiritual Life Coach',
+            title: 'Groomsman • Muse • Spiritual Life Coach',
             asset: 'christophe'
         }, {
             name: 'Elliott Thoreby',
-            title: 'Groomsman, Pioneer, Brewmaster, Jedi',
+            title: 'Groomsman • Pioneer • Brewmaster • Jedi',
             asset: 'elliott'
         }, {
             name: 'Josh Powell',
-            title: 'Groomsman, Role Model, Jiminy Cricket, Long Lost Akali Hyper-Carry',
+            title: 'Groomsman • Role Model • Jiminy Cricket • Long Lost Akali Hyper-Carry',
             asset: 'josh'
         }, {
             name: 'Tim Harris',
-            title: 'Groomsman, Confidant, Tim Harris, Horn of Mercury',
+            title: 'Groomsman • Confidant • Tim Harris • Horn of Mercury',
             asset: 'tim'
         }, {
             name: 'Dalrek Davis',
-            title: 'Groomsman, Brother to Be, Master of the Arts, AMF',
+            title: 'Groomsman • Brother to Be • Master of the Arts • AMF',
             asset: 'dalrek'
         }];
-
-        vm.bioUrl = function bioUrl (person) {
-            return './views/weddingParty/bios/' + person + '.txt';
-        };
-
-        vm.picUrl = function bioUrl (person) {
-            return './pics/' + person + '.jpeg';
-        };
     }
 
 })();
