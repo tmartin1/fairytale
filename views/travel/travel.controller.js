6c284e57a6246bdaca95d36a5b16cb88c7240084
Tim <timothy.m.martin@gmail.com>
2016-02-05 18:35:47 -0500

    Add initial travel info.

--- views/travel/travel.controller.js
+++ views/travel/travel.controller.js
@@ -6,6 +6,27 @@
 
     function travelController () {
         var vm = this;
+        // vm.currentView = currentView;
+
+        vm.tabs = [{
+            name: 'hotels',
+            label: 'Hotels',
+            url: './views/travel/tabs/hotels.html'
+        }, {
+            name: 'gettingAround',
+            label: 'Getting Around',
+            url: './views/travel/tabs/gettingAround.html'
+        }, {
+            name: 'parks',
+            label: 'Disney World!',
+            url: './views/travel/tabs/parks.html'
+        }];
+
+        vm.currentTab = vm.tabs[0];
+
+        // function currentView () {
+        //     return vm.tabs[vm.currentTab];
+        // }
     }
 
 })();
