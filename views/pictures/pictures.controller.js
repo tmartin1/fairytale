5b6fc58d56ce7a1a38fede2aa33294f465981c57
Tim Martin <timothy.m.martin@gmail.com>
2016-02-25 14:11:20 -0500

    Hacky workaround to get pics to scale in slideshow.

--- views/pictures/pictures.controller.js
+++ views/pictures/pictures.controller.js
@@ -4,7 +4,7 @@
     angular.module('fairytale.pictures', [])
         .controller('picturesController', picturesController);
 
-    function picturesController ($rootScope) {
+    function picturesController ($rootScope, $timeout) {
         var vm = this;
 
         vm.pics = new Array(21);
@@ -26,6 +26,27 @@
             vm.slideshowActive = false;
             $rootScope.viewConfig.scrollLock = false;
         };
+
+        // Maximize slideshow image size... in a shitty hacky way...
+        var scaled = 0;
+        function scalePics () {
+            console.log('scaling images');
+            var pics = angular.element('img');
+            scaled++;
+            angular.forEach(pics, function (pic) {
+                pic = angular.element(pic);
+                if (pic.hasClass('slideshow-image')) {
+                    var height = pic[0].naturalHeight;
+                    var width = pic[0].naturalWidth;
+                    var layout = height > width ? 'portrait' : 'landscape';
+                    pic.addClass(layout);
+                    if ((height === 0 || width === 0) && scaled < 5) {
+                        $timeout(scalePics, 1000);
+                    }
+                }
+            });
+        }
+        $timeout(scalePics, 1000);
     }
 
 })();
