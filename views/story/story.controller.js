c3ac28fc70f97d940ca8bb360e087928d6db7ec1
Tim Martin <timothy.m.martin@gmail.com>
2016-03-08 22:03:11 -0500

    Add our story.

--- views/story/story.controller.js
+++ views/story/story.controller.js
@@ -4,8 +4,44 @@
     angular.module('fairytale.story', [])
         .controller('storyController', storyController);
 
-    function storyController () {
+    function storyController ($log, $uibModal) {
         var vm = this;
+        vm.zoomIn = zoomIn;
+
+        vm.storyPics = [{
+            src: '1.jpg',
+            title: 'Our First Picture',
+            date: moment('20050817').format('LL'),
+            caption: 'Totes adorbs.'
+        }];
+
+        function zoomIn (pic) {
+            console.log('zoom zoom');
+
+            var modalInstance = $uibModal.open({
+                animation: false,
+                template: '<div class="zoomedPic">' +
+                    '<h2>' + pic.title + '</h2>' +
+                    '<img src="./pics-of-us/' + pic.src + '"></img>' +
+                    '<br><br>' +
+                    '<p>' + pic.date + '</p>' +
+                    '<p>' + pic.caption + '</p>' +
+                    '</div>',
+                controller: 'storyController',
+                size: 'lg',
+                resolve: {
+                    // items: function () {
+                    //     return $scope.items;
+                    // }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                // $scope.selected = selectedItem;
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
     }
 
 })();
