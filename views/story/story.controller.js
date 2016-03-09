02e864960335cb1c6ca0a67b05a028db3ae156e7
Tim Martin <timothy.m.martin@gmail.com>
2016-03-09 00:12:43 -0500

    Add and organize the pictures for our story.

--- views/story/story.controller.js
+++ views/story/story.controller.js
@@ -8,7 +8,7 @@
         var vm = this;
         vm.zoomIn = photoService.zoomIn;
 
-        vm.storyPics = photoService.getPics([0,1]);
+        vm.storyPics = photoService.getPics([0, 1, 14, 17, 26, 29, 39, 46, 47, 48]);
     }
 
 })();
